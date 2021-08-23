2171542ee3117664ebc3e580947895017f2cd34c
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-23 16:00:13 -0300

    refactor(structs): take Rectangle type as argument to show intent

--- src/structs/structs_test.go
+++ src/structs/structs_test.go
@@ -9,11 +9,11 @@ import (
 func Test_Perimeter(t *testing.T) {
 	t.Parallel()
 
-	assert.Equal(t, 40.0, Perimeter(10.0, 10.0))
+	assert.Equal(t, 40.0, Perimeter(Rectangle{Width: 10.0, Height: 10.0}))
 }
 
 func Test_Area(t *testing.T) {
 	t.Parallel()
 
-	assert.Equal(t, 72.0, Area(12.0, 6.0))
+	assert.Equal(t, 72.0, Area(Rectangle{Width: 12.0, Height: 6.0}))
 }
