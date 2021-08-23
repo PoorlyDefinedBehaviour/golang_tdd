f078bb1f3b4c3291cbb2a950791d464eb0a1cfdb
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-23 16:47:23 -0300

    feat(structs): methods instead of free functions

--- src/structs/structs.go
+++ src/structs/structs.go
@@ -1,14 +1,24 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rect Rectangle) float64 {
+func (rect *Rectangle) Perimeter() float64 {
 	return 2 * (rect.Width + rect.Height)
 }
 
-func Area(rect Rectangle) float64 {
+func (rect *Rectangle) Area() float64 {
 	return rect.Width * rect.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (circle *Circle) Area() float64 {
+	return math.Pi * circle.Radius * circle.Radius
 }
--- src/structs/structs_test.go
+++ src/structs/structs_test.go
@@ -6,14 +6,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Perimeter(t *testing.T) {
+func Test_RectanglePerimeter(t *testing.T) {
 	t.Parallel()
 
-	assert.Equal(t, 40.0, Perimeter(Rectangle{Width: 10.0, Height: 10.0}))
+	rect := Rectangle{Width: 10.0, Height: 10.0}
+
+	assert.Equal(t, 40.0, rect.Perimeter())
 }
 
-func Test_Area(t *testing.T) {
+func Test_RectangleArea(t *testing.T) {
 	t.Parallel()
 
-	assert.Equal(t, 72.0, Area(Rectangle{Width: 12.0, Height: 6.0}))
+	rect := Rectangle{Width: 12.0, Height: 6.0}
+
+	assert.Equal(t, 72.0, rect.Area())
+}
+
+func Test_CircleArea(t *testing.T) {
+	t.Parallel()
+
+	circle := Circle{Radius: 10.0}
+
+	assert.Equal(t, 314.1592653589793, circle.Area())
 }
