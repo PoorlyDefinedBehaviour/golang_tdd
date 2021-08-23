51a7a5f6518dcf1036c7a593326b7e7228d6be46
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-23 11:48:59 -0300

    feat(integers): Add example

--- src/integers/integers_test.go
+++ src/integers/integers_test.go
@@ -1,6 +1,7 @@
 package integers
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,4 +11,9 @@ func Test_Add(t *testing.T) {
 	t.Parallel()
 
 	assert.Equal(t, 4, Add(2, 2))
+}
+
+func ExampleAdd() {
+	fmt.Println(Add(1, 5))
+	// Output: 6
 }
