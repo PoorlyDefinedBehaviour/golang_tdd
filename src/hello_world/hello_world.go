4842fd10e486675f272594dc5d3717bf5acde22d
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-23 11:06:50 -0300

    feat(helloworld): handle empty name

--- src/hello_world/hello_world.go
+++ src/hello_world/hello_world.go
@@ -3,5 +3,9 @@ package helloworld
 import "fmt"
 
 func Hello(name string) string {
+	if name == "" {
+		return "Hello, world"
+	}
+
 	return fmt.Sprintf("Hello, %s", name)
 }
--- src/hello_world/hello_world_test.go
+++ src/hello_world/hello_world_test.go
@@ -9,5 +9,28 @@ import (
 func TestHello(t *testing.T) {
 	t.Parallel()
 
-	assert.Equal(t, "Hello, John", Hello("John"))
+	tests := []struct {
+		message  string
+		name     string
+		expected string
+	}{
+		{
+			message:  "says hello to the name passed in ",
+			name:     "John",
+			expected: "Hello, John",
+		},
+		{
+			message:  "returns the default message if name is empty",
+			name:     "",
+			expected: "Hello, worl1d",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Log(tt.message)
+
+		actual := Hello(tt.name)
+
+		assert.Equal(t, tt.expected, actual)
+	}
 }
