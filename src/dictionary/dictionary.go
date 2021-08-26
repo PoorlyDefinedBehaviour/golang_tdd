b7103e5e80606fa727a97d48313e4de3ac725f1c
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-25 23:04:01 -0300

    feat(dictionary): add Contains(string) bool

--- src/dictionary/dictionary.go
+++ src/dictionary/dictionary.go
@@ -5,3 +5,9 @@ type T map[string]string
 func (dict *T) Search(word string) string {
 	return (*dict)[word]
 }
+
+func (dict *T) Contains(word string) bool {
+	_, ok := (*dict)[word]
+
+	return ok
+}
--- src/dictionary/dictionary_test.go
+++ src/dictionary/dictionary_test.go
@@ -19,3 +19,40 @@ func Test_Search(t *testing.T) {
 
 	assert.Equal(t, expected, actual)
 }
+
+func Test_Contains(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		dict     T
+		key      string
+		expected bool
+	}{
+		{
+			dict:     T{},
+			key:      "a",
+			expected: false,
+		},
+		{
+			dict: T{
+				"a": "b",
+			},
+			key:      "c",
+			expected: false,
+		},
+		{
+			dict: T{
+				"a": "b",
+			},
+			key:      "a",
+			expected: true,
+		},
+	}
+
+	for _, tt := range tests {
+		actual := tt.dict.Contains(tt.key)
+
+		assert.Equal(t, tt.expected, actual)
+	}
+
+}
