466fd2d81afd4a5341e68524d27274fe583d01ce
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-27 17:49:41 -0300

    refactor(dictionary): maps already pointers to runtime.hmap

--- src/dictionary/dictionary.go
+++ src/dictionary/dictionary.go
@@ -2,16 +2,16 @@ package dictionary
 
 type T map[string]string
 
-func (dict *T) Search(key string) string {
-	return (*dict)[key]
+func (dict T) Search(key string) string {
+	return dict[key]
 }
 
-func (dict *T) Contains(key string) bool {
-	_, ok := (*dict)[key]
+func (dict T) Contains(key string) bool {
+	_, ok := dict[key]
 
 	return ok
 }
 
-func (dict *T) Remove(key string) {
-	delete(*dict, key)
+func (dict T) Remove(key string) {
+	delete(dict, key)
 }
