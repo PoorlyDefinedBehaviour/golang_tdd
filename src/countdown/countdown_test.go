5c080e92da454a1f92f934719cdd96ca4bc02bfa
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-27 18:17:48 -0300

    feat(countdown): ensure operations happen in the expected order

--- src/countdown/countdown_test.go
+++ src/countdown/countdown_test.go
@@ -15,20 +15,60 @@ func (sleeper *SleeperSpy) Sleep() {
 	sleeper.Calls++
 }
 
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+const (
+	write = "write"
+	sleep = "sleep"
+)
+
+func (spy *CountdownOperationsSpy) Sleep() {
+	spy.Calls = append(spy.Calls, sleep)
+}
+
+func (spy *CountdownOperationsSpy) Write(p []byte) (int, error) {
+	spy.Calls = append(spy.Calls, write)
+	return 0, nil
+}
+
 func TestCount(t *testing.T) {
 	t.Parallel()
 
-	buffer := bytes.Buffer{}
+	t.Run("writes expected values", func(t *testing.T) {
+		t.Parallel()
 
-	sleeperSpy := SleeperSpy{}
+		buffer := bytes.Buffer{}
 
-	Count(&buffer, &sleeperSpy)
+		sleeperSpy := SleeperSpy{}
 
-	expected := `3
-2
-1
-Go!`
+		Count(&buffer, &sleeperSpy)
 
-	assert.Equal(t, expected, buffer.String())
-	assert.Equal(t, 4, sleeperSpy.Calls)
+		expected := "3\n2\n1\nGo!"
+
+		assert.Equal(t, expected, buffer.String())
+		assert.Equal(t, 4, sleeperSpy.Calls)
+	})
+
+	t.Run("sleeps before every print", func(t *testing.T) {
+		t.Parallel()
+
+		spy := CountdownOperationsSpy{}
+
+		expected := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		Count(&spy, &spy)
+
+		assert.Equal(t, expected, spy.Calls)
+	})
 }
