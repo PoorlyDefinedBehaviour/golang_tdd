56841c82d55ca893441ce12ae79cbdf48b22990c
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-27 18:56:26 -0300

    refactor(concurrency): check urls concurrently

--- src/concurrency/concurrency.go
+++ src/concurrency/concurrency.go
@@ -5,10 +5,25 @@ type WebsiteChecker func(string) bool
 // returns a map where each key is a url and
 // the value is true if the url could be reached
 func CheckWebsites(checker WebsiteChecker, urls []string) map[string]bool {
+	type result struct {
+		string
+		bool
+	}
+
 	results := make(map[string]bool, len(urls))
+	resultsChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = checker(url)
+		url := url
+
+		go func() {
+			resultsChannel <- result{url, checker(url)}
+		}()
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultsChannel
+		results[result.string] = result.bool
 	}
 
 	return results
