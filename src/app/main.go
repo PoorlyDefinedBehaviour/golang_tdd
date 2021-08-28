216c8be6123cd39bc825ca565262d85c77cce894
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-08-28 19:51:42 -0300

    feat(app): start server

--- src/app/main.go
+++ src/app/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"golang_tdd/src/app/server"
+	"log"
+	"net/http"
+)
+
+func main() {
+	handler := http.HandlerFunc(server.PlayerServer)
+	log.Println("starting server")
+	log.Fatal(http.ListenAndServe(":5000", handler))
+}
